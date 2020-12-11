cc999a6c63690adb2977e2c8767a1eb0d6701223
Cesare Bek <cesarebek1@gmail.com>
2020-12-11 21:42:07 +0100

    3. Completed CRUD requests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
--- src/routes/location.js
+++ src/routes/location.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Location = require('../models/location');
 const upload = require('../utils/multer');
 const route = new express.Router();
+const _ = require('lodash');
 
 //Create location
 route.post('/', upload.single('image'), async (req, res) => {
@@ -15,17 +16,19 @@ route.post('/', upload.single('image'), async (req, res) => {
   });
   try {
     const saveLocation = await location.save();
-    res.status(201).json(saveLocation);
+    res
+      .status(201)
+      .json({ message: 'Location created successfully!', result: location });
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).json({ message: e });
   }
 });
 
 //Read locations
 route.get('/', async (req, res) => {
   try {
-    const locations = await Location.find({});
-    res.json(locations);
+    const allLocations = await Location.find({});
+    res.json({ pollutedLocations: allLocations });
   } catch (e) {
     res.status(500).json({ message: 'Unable to send back locations' });
   }
@@ -36,9 +39,70 @@ route.get('/:id', async (req, res) => {
   const id = req.params.id;
   try {
     const location = await Location.findById(id);
-    res.json(location);
+    if (!location) {
+      return res.status(404).json({ message: 'Location not found.' });
+    }
+    res.json({ pollutedLocation: location });
   } catch (e) {
-    res.status(404).send(e);
+    res.status(500).json({ message: e });
+  }
+});
+
+//Update location info
+route.patch('/update-info/:id', upload.none(), async (req, res) => {
+  try {
+    const location = await Location.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!location) {
+      return res.status(404).json({ message: 'Location not found.' });
+    }
+    res.json({
+      message: 'Location info updated successfully!',
+      result: location,
+    });
+  } catch (e) {
+    res.status(400).json({ message: e });
+  }
+});
+
+//Update location image
+route.patch('/update-image/:id', upload.single('image'), async (req, res) => {
+  //Url constructor for images link
+  const protocol = req.protocol;
+  const host = req.get('host');
+  const url = `${protocol}://${host}/`;
+  try {
+    const location = await Location.findByIdAndUpdate(
+      req.params.id,
+      { image: `${url}${req.file.path}` },
+      { new: true }
+    );
+    if (!location) {
+      res.status(404).json({ message: 'Location not found.' });
+    }
+    res.json({
+      message: 'Location image updated successfully!',
+      result: location,
+    });
+  } catch (e) {
+    res.status(400).json({ message: e });
+  }
+});
+
+route.delete('/:id', async (req, res) => {
+  try {
+    const location = await Location.findByIdAndDelete(req.params.id);
+    if (!location) {
+      return res.status(404).json({ message: 'Location not found.' });
+    }
+    res.json({
+      message: 'Location image deleted successfully!',
+      result: location,
+    });
+  } catch (e) {
+    res.status(500).json({ message: e });
   }
 });
 module.exports = route;
