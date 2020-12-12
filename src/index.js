ff10da6eb60f34342ce6e9bcd4c5c23daf8be634
Cesare Bek <cesarebek1@gmail.com>
2020-12-12 18:33:27 +0100

    Installed cors package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require('cors');
 const locationRoute = require('./routes/location');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.enable('trust proxy');
 app.use(express.json());
 app.use('/src/uploads', express.static('src/uploads'));
