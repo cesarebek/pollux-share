acc280d081d63fbd41f2fe50831ed9bde70646b7
Cesare Bek <cesarebek1@gmail.com>
2020-12-12 18:05:32 +0100

    Updated location model

--- src/models/location.js
+++ src/models/location.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const locationSchema = mongoose.Schema({
-  position: {
+  country: {
     type: String,
     required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  street_number: {
+    type: Number,
+    required: false,
   },
   image: {
     type: String,
